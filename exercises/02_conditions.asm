8a5efde7ee310a0f8052dfd5f0cf26b70f70d04c
Axel Viala <axel.viala@darnuria.eu>
2019-06-12 16:52:24 +0200

    📠 RISC-V Programming (Workshop 1)

---
+++ exercises/02_conditions.asm
@@ -0,0 +1,61 @@
+# Exercise 2
+# Notions:
+#
+# - be able to make a conditional jump in a program here.
+# - test the equality between two registers.
+# - carry out a conditional then unconditional branching.
+# - then display a message according to the result.
+
+# Objective of the program:
+#
+# Assign numbers in registers:
+# - `t0` <- 5
+# - `t1` <- 4
+#
+# Correct the lines with "???".
+.data
+
+msg_eq: .asciz "These numbers are equal!"
+msg_neq: .asciz "These numbers are not equal"
+
+.text
+
+# In rust we would have surely done something like this
+# ```rust
+# let a = 4;
+# let b = 5;
+# let msg = if a == b { "These numbers are equal!" }
+#                else { "These numbers are not equal" };
+# println!(msg);
+# ```
+
+### Here you can use
+# two instructions to write to load 5 into t0 and t1 into 4.
+
+# start of the program.
+main:
+li t0, 4
+li t1, 5
+
+# `bne` Branch if Not Equals: Jump if our registers are not equal.
+bne t0, t1, not_eq # if t0! = t1 jump not_eq
+
+# Equal cases t0 == t1
+la ???, ??? # msg <- "These numbers are equal!"
+j end # We are done so we can get out of the if.
+
+# Unequal cases t0 != T1
+not_eq:
+la ???, ??? # msg <- "These numbers are not equal"
+###
+
+end:
+
+# Display:
+#
+# Here we want to display on the console via a system call.
+# Take inspiration from 01_syscall, register a0 must contain the number of the
+# syscall printString: 4 and a7 the string to display.
+
+li ???, 4 # syscall number
+???       # which instruction should we use to make a syscall?
---
+++ exercises/04_array.asm
@@ -0,0 +1,75 @@
+# Exercise 4
+#
+# Objective: add all the cells of a table version "naive"
+# Without a loop, this corresponds to adding array_g[0] + array_g[1] + array_g[2].
+#
+# TODO: complete the hole program.
+#
+# Questions:
+#
+# - What does the `lw` (load word) instruction do?
+# - lw: What is the immediate use in front of the register containing the address?
+# - What will lw's destination register contain?
+#
+# - With an instruction like `add` what do the three registers correspond to?
+# `add reg0, reg1, reg2`
+# - reg0:
+# - reg1:
+# - reg2:
+#
+# - `la` what is it for?
+
+.data
+# /!\ Warning! they are global variables in memory! /!\
+
+# We declare "naively" an array and its size, in the data segment.
+# We will see in the rest of the course that this is not exactly how we do it,
+# in real life do things in the stack.
+size_array_g: .word 3
+array_g: .word 124, 256, 512
+
+# In memory it looks like this @ refer in this diagram to the address
+# base of the array.
+# + ------- + ----- + ----- + ----- +
+# | Address | @ + 0 | @ + 4 | @ + 8 |
+# + ------- + ----- + ----- + ----- +
+# | Content | 124   | 256   | 512   |
+# + ------- + ----- + ----- + ----- +
+
+# Strings for testing
+msg_success: .asciz "The sum matches well! Well done."
+msg_fail: .asciz "Whops it's not that yet, courage! :)"
+
+.text
+
+main:
+
+la t0, array_g # t0 contains the address of the array
+
+# To sum the array we will add manually, the content of all the cells of the
+# table.
+
+lw t1, 0​​(t0) # t1: array_g[0]
+# lw ???, 4(???) <- TODO: To be completed!
+# You can also add on the address which is in t0
+add a0, t1, t2 # sum = t2 + t1
+
+
+# Here you have to add a0 which contains the sum with the register which will
+# contain the value of the 3rd cell of the table.
+
+######################################################################
+# Teacher part: test if it works and displays a message accordingly. #
+######################################################################
+test:
+# Test sum == 892
+li  a1, 892
+bne a0, a1, fail_test # if a0 !t1 then fail_test
+la a0, msg_success
+j print
+fail_test:
+la a0, msg_fail
+
+print:
+li a7, 4
+ecall
