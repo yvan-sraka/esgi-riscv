5ed22bf2e1fa8f3bda9eeb969007e15f9f068188
Cpasmoi91560 <corentin.delage@hotmail.fr>
2021-04-14 09:53:19 +0200

    exo 1ere session

--- exercises/03_memory.asm
+++ exercises/03_memory.asm
@@ -2,35 +2,35 @@
 #
 # Objective: Play with integers in memory to start!
 #
-# TODO: complete the hole program.
+# To do: complete the hole program.
 #
 # Questions:
 #
-# - What's the address of `num0`,` num1`, `result`?
-# - What's the last code address of your program?
-# - How is instruction `lw t1, 0​​(t0)` quoted in machine language (hex)?
-# - Is memory (not registers) in the processor?
+# - What is the address of `num0`,` num1`, `result`?
+# - The last code address of your program
+# - How is the instruction quoted in machine language (hexadecimal): `lw t1, 0​​(t0)`
+# - Is memory in the processor?
 # - When you access the memory by 32-bit words, what's the step?
-# - What's the value in memory of the value at address `num0`?
+# - What is the value in memory of the value at address `num0`
 
 # Imagine a C program like this: (available in 03_memory.c)
 #
 # ```C
-# #include <stdint.h>
-# // We ask the compiler to * not optimize * the memory access with `volatile`.
+# // We ask the compiler to * not optimize * the
+# // memory access with `volatile`.
 # // In embedded or kernel it is sometimes essential.
 # // The rest of the time it's counterproductive. ;)
 # // `static` indicates the variable is local to the file.
 # static volatile int32_t num0 = 124;
 # static volatile int32_t num1 = 256;
 # static volatile int32_t result_g = 0;
-
+#
 # void main(void) {
-#   // Note that your language hides the use of pointers here, in reality we
-#   // are manipulating memory here (especially if no optimization takes place)
-#   result_g = num0 + num1;
-#   printf("% d", result_g);
-# }
+# // Note that your language hides the use of pointers here,
+# // actually we are manipulating memory here
+# // (especially if no optimization takes place).
+# result_g = num0 + num1;
+#}
 # ```
 # We declare two 32-bit numbers in * memory *
 # .word allows you to specify that you want a 32-bit word.
@@ -48,21 +48,23 @@ result_g: .word 0
 main:
 
 # ??? <- @ num0
-l? ???, num0_g
-# Load word from address 0 + register in register t1
-lw t1, 0​​(t0)
+la t0, num0_g
+# load word from address 0 + register in register t1
+lw t1, (t0)
 
 # t2 <- @ num1
-l? t2, ???
+la t2, num1_g
 # Which instruction is used to load a memory word?
-??? t3, 0(t2)
+lw t3, (t2)
 
-# Loading of the address of the result.
-la t4, ???
-# Make the addition between our two registers, saving the result in another register.
-add ???, ???, ???
-# Store word: store the contents of the register in the address at 0 + t4
-sw ???, 0(t4)
+# loading of the address of the result.
+la t4, result_g
+# make the addition between our two registers in one register.
+add t5, t1, t3
+# store word, store the contents of the register in the address at 0 + t4
+sw t5, 0(t4)
 
-# TODO: Display our two numbers in the console!
-# Hint: syscall printInt
+# To be done by you totally, display our two numbers in the console (hint: syscall printInt)
+la a0, result_g
+li a7,1
+ecall
--- exercises/03_memory.c
+++ exercises/03_memory.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
-// We ask the compiler to * not optimize * the memory access with `volatile`.
+// We ask the compiler to * not optimize * the
+// memory access with `volatile`.
 // In embedded or kernel it is sometimes essential.
 // The rest of the time it's counterproductive. ;)
 // `static` indicates the variable is local to the file.
@@ -8,8 +9,9 @@ static volatile int32_t num1 = 256;
 static volatile int32_t result_g = 0;
 
 void main(void) {
-   // Note that your language hides the use of pointers here, in reality we are
-   // manipulating memory here (especially if no optimization takes place).
+   // Note that your language hides the use of pointers here,
+   // in reality we are manipulating memory here
+   // (especially if no optimization takes place).
    result_g = num0 + num1;
    printf("% d", result_g);
 }
