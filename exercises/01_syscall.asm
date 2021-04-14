99e1abbfb821490ab204bd6e0abb7f89078907ea
Thibma <balsamothibault@hotmail.fr>
2021-04-14 09:56:21 +0200

    replace exercices already done yesterday

--- exercises/01_syscall.asm
+++ exercises/01_syscall.asm
@@ -10,21 +10,25 @@
 # We indicate that we want to store data in the data segment.
 .data
 
-# the .asciz directive allows you to declare a string ending with a 0x00, '\0'
-# in ASCII.
+# the .asciz directive allows you to declare a string ending with a 0x00, '\0' in ASCII.
 message: .asciz "Hello ASM from .data"
-
 .text # <- code segment.
 
 # Exercise: Observation in RARS
 #
-# - What is the base address of the msg chain? Its end address?
+# - What is the base address of the msg chain? His end address?
+#   Base : 0x6c6c6548 | End : 0x61746164
 # - In which segment of memory is it?
+#   0x10010000
 # - What are the hexadecimal values ​​present in .data?
+#   Ce sont les valeurs en Hexadecimal de 4 caractères en ASCII
 
 # la: Load Address, load an address (label) into a register
-la a0, ??? # argument of the system call
-li a7, ??? # system call number 4 according to the RARS printString doc
-ecall      # used to switch to kernel mode to make the system call
+la a0, message # argument of the system call
+li a7, 4  # system call number 4 according to the RARS printString doc
+ecall       # used to switch to kernel mode to make the system call
 
 # Bonus: Now display a number, you will need the documentation.
+li a0, 64
+li a7, 1
+ecall
